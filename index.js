73132b09bd784a8d32332813412df983c0f8f097
bakarr86 <bakarr19861@gmail.com>
2024-09-15 18:21:49 -0400

    middleware

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ const bodyParser= require('body-parser');
 const connectDB= require('./mongo/connect.jsx')
 const morgan=require('morgan');
 const methodOverride = require('method-override');
+
+//middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.set('views', __dirname + '/views');
