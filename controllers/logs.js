fc4c6e1cdd6629ed3efe9f46a251f8d98af0e97f
bakarr86 <bakarr19861@gmail.com>
2024-09-15 18:12:42 -0400

    lists of comment route

--- controllers/logs.js
+++ controllers/logs.js
@@ -1,3 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const Log = require('./models/logs.js');
+
+// lits of comment Route
+router.get('/', async (req, res) => {
+    try {
+    const logs = await Log.find();
+    res.render('Index', { logs });
+    } catch (error) {
+    console.log(error);
+    res.status(500).send('Internal Server Error');
+    }
+    });
+    
+    // New Route
+    router.get('/new', (req, res) => {
+    res.render('New');
+    });
