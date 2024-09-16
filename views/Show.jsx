78bcd36a6a174e04c277aa19550219452f3d92dd
bakarr86 <bakarr19861@gmail.com>
2024-09-16 06:41:12 -0400

    div container

--- views/Show.jsx
+++ views/Show.jsx
@@ -2,4 +2,21 @@
 const React = require('react');
 
 const Show = ({ log }) => (
-  <div></div>
+  //div container
+  <div>
+
+  <div className="container">
+      <h1 className="title">Captain's Log - {log.title}</h1>
+      <p>Entry: {log.entry}</p>
+      <p>Ship is Broken: {log.shipIsBroken ? 'Yes' : 'No'}</p>
+
+      <div className="action-buttons">
+        <button><a href={`/logs/delete/${log._id}`}>Delete</a></button> 
+        <a href="/logs" className="create-link">Back to Index</a>
+        <button><a href={`/logs/edit/${log._id}`}>Update</a></button>
+      </div>
+    </div>
+  </div>
+);
+
+module.exports = Show;
