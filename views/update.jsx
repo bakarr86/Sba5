75044db40fc207e945ba3377a195f81fa5462755
bakarr86 <bakarr19861@gmail.com>
2024-09-16 06:45:57 -0400

    update completed

--- views/update.jsx
+++ views/update.jsx
@@ -10,3 +10,28 @@ const NewUpdate = ({ myOutput }) => {
   const handleCheckboxChange = () => {
     setShipIsBroken(!shipIsBroken);
   };
+//update completed
+  return (
+    <div>
+      <h1>Create New Log</h1>
+      <form action={`/logs/update/${myOutput.id}`} method="POST">
+        <label>
+          Title: <input type="text" name="title" defaultValue={myOutput.title} />
+        </label>
+        <br />
+        <label>
+          Entry: <textarea name="entry" defaultValue={myOutput.entry}></textarea>
+        </label>
+        <br />
+        <label>
+          Ship is Broken: <input type="checkbox" name="shipIsBroken" defaultChecked={myOutput.shipIsBroken} onChange={handleCheckboxChange} />
+        </label>
+        <br />
+        <input type="submit" value="Update Log" />
+      </form>
+      <a href="/logs">Back to Index</a>
+    </div>
+  );
+};
+
+module.exports = NewUpdate;
